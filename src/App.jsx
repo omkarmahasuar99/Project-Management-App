97e7dedb663be0bc1bb4577f6698c79b4b9eec29
omkarmahasuar99 <126793930+omkarmahasuar99@users.noreply.github.com>
2024-03-26 19:29:16 +0530

    add : Additional Components

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,34 @@
+import NewProject from "./components/NewProject";
+import NoProjectSelected from "./components/NoProjectSelected";
+import ProjectsSidebar from "./components/ProjectsSidebar";
+import { useState } from "react";
 function App() {
+  const [projectsState, setProjectsState] = useState({
+    selectedProjectId: undefined,
+    projects: [],
+  });
+
+  let content;
+  if (projectsState.selectedProjectId === undefined) {
+    content = <NoProjectSelected onStartAddProject={handleStartAddProject} />;
+  } else if (projectsState.selectedProjectId === null) {
+    content = <NewProject />;
+  }
+
+  function handleStartAddProject() {
+    setProjectsState((prevState) => {
+      return {
+        ...prevState,
+        selectedProjectId: null,
+      };
+    });
+  }
+
   return (
-    <>
-      <h1 className="my-8 text-center text-5xl font-bold">Hello World</h1>
-    </>
+    <main className="h-screen my-8 flex gap-8">
+      <ProjectsSidebar onStartAddProject={handleStartAddProject} />
+      {content}
+    </main>
   );
 }
 
